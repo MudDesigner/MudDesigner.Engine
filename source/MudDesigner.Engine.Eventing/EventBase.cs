8f000588f1302099f589a3c58b825479cdcfcc67
Johnathon Sullinger <Johnathon@DiversionSoftware.com>
2018-10-13 20:41:10 -0700

    Initial implementation of a local event dispatcher.

---
+++ source/MudDesigner.Engine.Eventing/EventBase.cs
@@ -0,0 +1,22 @@
+﻿namespace MudDesigner.Engine.Eventing
+{
+    /// <summary>
+    /// Provides methods for dispatching notifications to subscription handlers
+    /// </summary>
+    /// <typeparam name="TMessageType">The type of the message type.</typeparam>
+    public abstract class EventBase<TContentType> : IEvent<TContentType> where TContentType : class
+    {
+        /// <summary>
+        /// Gets the content of the message.
+        /// </summary>
+        public TContentType Content { get; protected set; }
+
+        /// <summary>
+        /// Gets the content.
+        /// </summary>
+        /// <returns>
+        /// Returns the content of the message
+        /// </returns>
+        public object GetContent() => this.Content;
+    }
+}
