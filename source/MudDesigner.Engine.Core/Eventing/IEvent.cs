07ad20977cf9535753602b3124b3b4907040d22b
Johnathon Sullinger <Johnathon@DiversionSoftware.com>
2018-10-13 20:39:51 -0700

    Initial contract for the eventing API.

---
+++ source/MudDesigner.Engine.Core/Eventing/IEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace MudDesigner.Engine.Eventing
+{
+    /// <summary>
+    /// Allows for receiving the content of a message
+    /// </summary>
+    public interface IEvent
+    {
+        /// <summary>
+        /// Gets the content.
+        /// </summary>
+        /// <returns>Returns the content of the message</returns>
+        object GetContent();
+    }
+}
